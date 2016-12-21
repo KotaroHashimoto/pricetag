a979866e88025edcbffab65c910f42a9175b919d
Kotaro Hashimoto <kotaro.hashimoto.jv@hitachi.com>
2016-12-22 00:22:06 +0900

    hoge
    Signed-off-by: Kotaro Hashimoto <kotaro.hashimoto.jv@hitachi.com>

--- cpOrderBook.py
+++ cpOrderBook.py
@@ -5,7 +5,7 @@ OANDA_DIR = 'C:\\Users\\Administrator\\AppData\\Roaming\\MetaQuotes\\Terminal\\3
 FXTF_DIR = 'C:\\Users\\Administrator\\AppData\\Roaming\\MetaQuotes\\Terminal\\3F58D636E9CFCB1149A0A8D4AE12E98D\\MQL4\\Files\\'
 RAKUTEN_DIR = 'C:\\Users\\Administrator\\AppData\\Roaming\\MetaQuotes\\Terminal\\3212703ED955F10C7534BE8497B221F4\\MQL4\\Files\\'
 
-pair = ['EURUSD', 'USDJPY', 'EURJPY', 'GBPUSD', 'GBPJPY', 'AUDJPY', 'NZDJPY', 'ZARJPY']
+pair = ['EURUSD', 'USDJPY', 'EURJPY', 'GBPUSD', 'GBPJPY', 'AUDJPY']
 
 
 for c in pair:
@@ -15,5 +15,5 @@ for c in pair:
     dst = FXTF_DIR + c + '.csv'
     copyfile(src, dst)
 
-    dst = RAKUDEN_DIR + c + '.csv'
-    copyfile(src, dst)
+#    dst = RAKUDEN_DIR + c + '.csv'
+#    copyfile(src, dst)
