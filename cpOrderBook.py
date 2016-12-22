1a1d336cfed2adc4a14d4e5a6be5ec61cd3c715b
KotaroHashimoto <hasimoto.kotaro@gmail.com>
2016-12-22 09:39:52 +0900

    hoge
    Signed-off-by: KotaroHashimoto <hasimoto.kotaro@gmail.com>

--- cpOrderBook.py
+++ cpOrderBook.py
@@ -1,7 +1,5 @@
-
 from time import sleep
 from shutil import copyfile
-
 
 def hasUpdated(currency, orderbook):
 
@@ -28,13 +26,12 @@ def hasUpdated(currency, orderbook):
 
 OANDA_DIR = 'C:\\Users\\Administrator\\AppData\\Roaming\\MetaQuotes\\Terminal\\3212703ED955F10C7534BE8497B221F4\\MQL4\\Files\\'
 FXTF_DIR = 'C:\\Users\\Administrator\\AppData\\Roaming\\MetaQuotes\\Terminal\\3F58D636E9CFCB1149A0A8D4AE12E98D\\MQL4\\Files\\'
-RAKUTEN_DIR = 'C:\\Users\\Administrator\\AppData\\Roaming\\MetaQuotes\\Terminal\\3212703ED955F10C7534BE8497B221F4\\MQL4\\Files\\'
 
 pair = ['EURUSD', 'USDJPY', 'EURJPY', 'GBPUSD', 'GBPJPY', 'AUDJPY']
 latest = {}
 
 while True:
-    sleep(10)
+    sleep(5)
 
     for c in pair:
         src = OANDA_DIR + 'OANDA_' + c + '.csv'
@@ -42,4 +39,3 @@ while True:
         if(hasUpdated(c, src)):
             dst = FXTF_DIR + c + '.csv'
             copyfile(src, dst)
-
